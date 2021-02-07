3c37af9c20a6cedc5340494509672dd449fa19d3
bjun626 <34598848+bjun626@users.noreply.github.com>
2021-02-07 19:51:09 +0800

    Re-invert display, remove unnecessary definition, less serial.println

--- src/qrcode.cpp
+++ src/qrcode.cpp
@@ -8,83 +8,96 @@ int screenwidth = 128;
 int screenheight = 64;
 bool QRDEBUG = false;
 
-QRcode::QRcode(Adafruit_SSD1306 *display){
-	this->display = display;
+QRcode::QRcode(Adafruit_SSD1306 *display)
+{
+  this->display = display;
 }
 
-void QRcode::init(){
-	if(QRDEBUG)
-		Serial.println("QRcode init");
-  display->invertDisplay(true);
+void QRcode::init()
+{
+  if (QRDEBUG)
+    Serial.println("QRcode init");
 }
 
-void QRcode::debug(){
-	QRDEBUG = true;
+void QRcode::debug()
+{
+  QRDEBUG = true;
 }
 
-void QRcode::render(int x, int y, int color){
-  x=x+offsetsX;
-  y=y+offsetsY;
-  if(color==1) {
-    display->drawPixel(x,y,BLACK);
+void QRcode::render(int x, int y, int color)
+{
+  x = x + offsetsX;
+  y = y + offsetsY;
+  if (color == 1)
+  {
+    display->drawPixel(x, y, BLACK);
   }
-  else {
-    
-    display->drawPixel(x,y,WHITE);
+  else
+  {
+    display->drawPixel(x, y, WHITE);
   }
 }
 
-void QRcode::screenwhite(){
-	display->clearDisplay();
-	display->fillRect(0, 0, screenwidth, screenheight,WHITE);
-	display->display();
+void QRcode::screenwhite()
+{
+  display->clearDisplay();
+  display->fillRect(0, 0, screenwidth, screenheight, WHITE);
+  display->display();
 }
 
-void QRcode::create(String message) {
+void QRcode::create(String message)
+{
 
   // create QR code
-  message.toCharArray((char *)strinbuf,260);
+  message.toCharArray((char *)strinbuf, 260);
   qrencode();
   screenwhite();
 
-  if(QRDEBUG){
-	Serial.println("QRcode render");
-	Serial.println();
-  }
-  // print QR Code
-  for (byte x = 0; x < WD; x+=2) {
-    for (byte y = 0; y < WD; y++) {
-      if ( QRBIT(x,y) &&  QRBIT((x+1),y)) {
-        // black square on top of black square
-        if(QRDEBUG)
-			Serial.print("@");
-        render(x, y, 1);
-        render((x+1), y, 1);
-      }
-      if (!QRBIT(x,y) &&  QRBIT((x+1),y)) {
-        // white square on top of black square
-        if(QRDEBUG)
-			Serial.print(" ");
-        render(x, y, 0);
-        render((x+1), y, 1);
-      }
-      if ( QRBIT(x,y) && !QRBIT((x+1),y)) {
-        // black square on top of white square
-        if(QRDEBUG)
-			Serial.print("@");
-        render(x, y, 1);
-        render((x+1), y, 0);
-      }
-      if (!QRBIT(x,y) && !QRBIT((x+1),y)) {
-        // white square on top of white square
-        if(QRDEBUG)
-			Serial.print(" ");
-        render(x, y, 0);
-        render((x+1), y, 0);
-      }
-    }
+  if (QRDEBUG)
+  {
+    Serial.println("QRcode render");
     Serial.println();
   }
-  Serial.println();
+  // print QR Code
+  for (byte x = 0; x < WD; x += 2)
+  {
+    for (byte y = 0; y < WD; y++)
+    {
+      if (QRBIT(x, y) && QRBIT((x + 1), y))
+      {
+        // black square on top of black square
+        if (QRDEBUG)
+          Serial.print("@");
+        render(x, y, 1);
+        render((x + 1), y, 1);
+      }
+      if (!QRBIT(x, y) && QRBIT((x + 1), y))
+      {
+        // white square on top of black square
+        if (QRDEBUG)
+          Serial.print(" ");
+        render(x, y, 0);
+        render((x + 1), y, 1);
+      }
+      if (QRBIT(x, y) && !QRBIT((x + 1), y))
+      {
+        // black square on top of white square
+        if (QRDEBUG)
+          Serial.print("@");
+        render(x, y, 1);
+        render((x + 1), y, 0);
+      }
+      if (!QRBIT(x, y) && !QRBIT((x + 1), y))
+      {
+        // white square on top of white square
+        if (QRDEBUG)
+          Serial.print(" ");
+        render(x, y, 0);
+        render((x + 1), y, 0);
+      }
+    }
+    //Serial.println();
+  }
+  //Serial.println();
   display->display();
 }
